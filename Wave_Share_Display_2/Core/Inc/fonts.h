c1c73c74d0039ff32e6cc1aab284eb2650f378b5
KAVATER <kushpetwal13@gmail.com>
2026-08-05 15:07:13 +0530

    Added 16Bit Font support

--- Wave_Share_Display_2/Core/Inc/fonts.h
+++ Wave_Share_Display_2/Core/Inc/fonts.h
@@ -4,12 +4,15 @@
 #include <stdint.h>
 
 
-#define FONT1  Arial16
-#define FONT2  Arial32
+//#define FONT1  Arial16
+//#define FONT2  Arial32
+#define FONT3  Arial56
+//#define FONT4  Arial64
 
-
-extern const uint16_t Arial16[];
-extern const uint16_t Arial32[];
+extern const uint16_t Arial56[];
+//extern const uint16_t Arial64[];
+//extern const uint16_t Arial16[];
+//extern const uint16_t Arial32[];
 
 
 #endif // __FONTS_H__
